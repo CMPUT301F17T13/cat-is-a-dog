3c5d6e6228872f0e67d7e2bf6ced16f9eb1d3e55
Nathan Liebrecht <nwliebrecht@gmail.com>
2017-10-22 15:40:40 -0600

    Nathan (#24)
    * asdf
    
    * tests

--- app/src/test/java/cmput301f17t13/com/catisadog/UserTest.java
+++ app/src/test/java/cmput301f17t13/com/catisadog/UserTest.java
@@ -4,6 +4,8 @@
 package cmput301f17t13.com.catisadog;
 
 import org.junit.Test;
+
+import java.util.HashMap;
 import cmput301f17t13.com.catisadog.models.user.User;
 import static org.junit.Assert.*;
 
@@ -15,5 +17,27 @@ public class UserTest {
         assertEquals(user.getUsername(), "username");
         assertTrue(user.getFollowers().size() == 0);
         assertTrue(user.getFollowing().size() == 0);
+    }
+
+    @Test
+    public void addFollowerFollowingUserTest() throws Exception {
+        User user = new User("main_user");
+
+        HashMap<String, Boolean> followers = new HashMap<>();
+        followers.put("follower1", true);
+        followers.put("follower2", true);
+        user.setFollowers(followers);
+
+        HashMap<String, Boolean> following = new HashMap<>();
+        following.put("following1", true);
+        following.put("following2", true);
+        following.put("following3", true);
+        user.setFollowing(following);
+
+        assertTrue(user.getFollowers().size() == 2);
+        assertTrue(user.getFollowing().size() == 3);
+
+        assertTrue(user.getFollowers().containsKey("follower1"));
+        assertTrue(user.getFollowers().containsKey("following1"));
     }
 }
