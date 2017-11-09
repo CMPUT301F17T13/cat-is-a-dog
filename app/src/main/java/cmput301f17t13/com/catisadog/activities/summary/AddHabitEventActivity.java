0c4346354d0263b2bf140185b227ead36d5b2230
Nathan Liebrecht <nwliebrecht@gmail.com>
2017-11-09 09:14:07 -0700

    added maps support and worked on adding habit events activity

--- app/src/main/java/cmput301f17t13/com/catisadog/activities/summary/AddHabitEventActivity.java
+++ app/src/main/java/cmput301f17t13/com/catisadog/activities/summary/AddHabitEventActivity.java
@@ -3,6 +3,13 @@ package cmput301f17t13.com.catisadog.activities.summary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import cmput301f17t13.com.catisadog.R;
 
 /**
@@ -12,11 +19,27 @@ import cmput301f17t13.com.catisadog.R;
  * @see HabitSummaryActivity
  * @see cmput301f17t13.com.catisadog.models.HabitEvent
  */
-public class AddHabitEventActivity extends AppCompatActivity {
+public class AddHabitEventActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_habit_event);
+
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
