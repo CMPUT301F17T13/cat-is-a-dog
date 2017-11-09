44297efcd7a58ce12b16b2c812c8492ba8084537
Nathan Liebrecht <nwliebrecht@gmail.com>
2017-11-08 19:25:51 -0700

    setup and created add habbit event activity

--- app/src/main/java/cmput301f17t13/com/catisadog/activities/summary/AddHabitEventActivity.java
+++ app/src/main/java/cmput301f17t13/com/catisadog/activities/summary/AddHabitEventActivity.java
@@ -1,28 +1,22 @@
-/*
- * Add Habit Event Fragment Activity
- *
- * Copyright 2017 James Hryniw, Leland Jansen, Nathan Liebrecht, Abid Rahman, Kevin Wang - All Rights Reserved.
- * Licensed under the Apache License, Version 2.0. Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- */
-
 package cmput301f17t13.com.catisadog.activities.summary;
 
-import android.support.v4.app.FragmentActivity;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cmput301f17t13.com.catisadog.R;
 
 /**
  * Screen for creating new habit events.
- * Child Fragment Activity of HabitSummaryActivity
+ * Child Activity of HabitSummaryActivity
  *
  * @see HabitSummaryActivity
  * @see cmput301f17t13.com.catisadog.models.HabitEvent
  */
+public class AddHabitEventActivity extends AppCompatActivity {
 
-public class AddHabitEventActivity extends FragmentActivity {
-
-    public void createHabitEvent(View v) {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_habit_event);
     }
-
 }
