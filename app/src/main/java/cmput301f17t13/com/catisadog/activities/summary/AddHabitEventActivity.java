adb7f698949875e304c246bdc77d12a432dcdb18
James Hryniw <jhryniw@gmail.com>
2017-11-11 15:43:44 -0700

    Login with Google using Firebase (#40)
    * added tab view
    
    * setup and created add habbit event activity
    
    * added maps support and worked on adding habit events activity
    
    * more work on add habit events
    
    * google firebase auth
    
    * authenticate user with firebase, user persistance #14 #39
    
    * more docs

--- app/src/main/java/cmput301f17t13/com/catisadog/activities/summary/AddHabitEventActivity.java
+++ app/src/main/java/cmput301f17t13/com/catisadog/activities/summary/AddHabitEventActivity.java
@@ -1,28 +1,70 @@
-/*
- * Add Habit Event Fragment Activity
- *
- * Copyright 2017 James Hryniw, Leland Jansen, Nathan Liebrecht, Abid Rahman, Kevin Wang - All Rights Reserved.
- * Licensed under the Apache License, Version 2.0. Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- */
-
 package cmput301f17t13.com.catisadog.activities.summary;
 
-import android.support.v4.app.FragmentActivity;
-import android.view.View;
+import android.net.Uri;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import cmput301f17t13.com.catisadog.R;
+import cmput301f17t13.com.catisadog.fragments.summary.AddOverlayFragment;
 
 /**
  * Screen for creating new habit events.
- * Child Fragment Activity of HabitSummaryActivity
+ * Child Activity of HabitSummaryActivity
  *
  * @see HabitSummaryActivity
  * @see cmput301f17t13.com.catisadog.models.HabitEvent
  */
+public class AddHabitEventActivity extends AppCompatActivity implements
+        OnMapReadyCallback,
+        AddOverlayFragment.OnInteractionListener {
 
-public class AddHabitEventActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_habit_event);
 
-    public void createHabitEvent(View v) {
-
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuItem menuItem = menu.add(Menu.NONE, 1000, Menu.NONE, "Save");
+        MenuItemCompat.setShowAsAction(menuItem, MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return true;
+    }
+
+    public void onClickAddOverlay(Uri uri) {
+
+    }
 }
