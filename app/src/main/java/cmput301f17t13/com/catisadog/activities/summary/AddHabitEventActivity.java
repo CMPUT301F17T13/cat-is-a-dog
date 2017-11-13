8f0be169f76b091ea6160d23655e75a58559837d
Nathan Liebrecht <nwliebrecht@gmail.com>
2017-11-12 19:45:32 -0700

    removed LoginActivityGoogle

--- app/src/main/java/cmput301f17t13/com/catisadog/activities/summary/AddHabitEventActivity.java
+++ app/src/main/java/cmput301f17t13/com/catisadog/activities/summary/AddHabitEventActivity.java
@@ -149,21 +149,15 @@ public class AddHabitEventActivity extends AppCompatActivity implements
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the
-                    // location-related task you need to do.
+                    // permission was granted, yay!
                     getLocation();
                 } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                    // permission denied, boo!
                     Toast.makeText(this, "Do not have permissions to get location!",
                             Toast.LENGTH_LONG).show();
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 
@@ -174,13 +168,6 @@ public class AddHabitEventActivity extends AppCompatActivity implements
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
                     MY_PERMISSIONS_REQUEST_LOCATION);
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
 
