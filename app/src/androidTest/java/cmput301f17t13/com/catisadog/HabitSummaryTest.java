9d60a1bb4aa8a4842ad90410c509aab83b0e4b3e
James Hryniw <jhryniw@gmail.com>
2017-12-03 01:52:31 -0700

    Fix CurrentUser schema in UI tests (#97)
    * fixed all ui tests and added mock auth
    
    * show habit test
    
    * view and todo habit tests
    
    * fixed tests for new Current User schema

--- app/src/androidTest/java/cmput301f17t13/com/catisadog/HabitSummaryTest.java
+++ app/src/androidTest/java/cmput301f17t13/com/catisadog/HabitSummaryTest.java
@@ -7,31 +7,21 @@
 package cmput301f17t13.com.catisadog;
 
 import android.os.SystemClock;
-import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.intent.Intents;
-import android.support.test.espresso.matcher.*;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
 
 import org.hamcrest.Matcher;
-import org.joda.time.DateTime;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.HashSet;
-
 import cmput301f17t13.com.catisadog.activities.summary.HabitSummaryActivity;
 import cmput301f17t13.com.catisadog.activities.summary.ViewHabitActivity;
 import cmput301f17t13.com.catisadog.mocks.MockAuthentication;
-import cmput301f17t13.com.catisadog.models.habit.Habit;
-import cmput301f17t13.com.catisadog.models.user.CurrentUser;
 
-import static android.support.test.espresso.DataInteraction.*;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -39,15 +29,10 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.hasValue;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
 public class HabitSummaryTest {
