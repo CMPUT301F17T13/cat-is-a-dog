c2437bf9122556da5d1bb0eb5eb7cfbffc34be8d
Nathan Liebrecht <nwliebrecht@gmail.com>
2017-12-03 21:55:03 -0700

    added integration for nearby habit history search

--- app/src/main/java/cmput301f17t13/com/catisadog/activities/history/HabitHistoryActivity.java
+++ app/src/main/java/cmput301f17t13/com/catisadog/activities/history/HabitHistoryActivity.java
@@ -1,14 +1,18 @@
 package cmput301f17t13.com.catisadog.activities.history;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +21,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,6 +34,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
 import java.util.Observable;
@@ -50,15 +58,21 @@ import cmput301f17t13.com.catisadog.utils.data.Repository;
 public class HabitHistoryActivity extends BaseDrawerActivity implements
         Observer, OnMapReadyCallback, FilterDialogResultListener {
 
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1111;
+
     private GoogleMap map;
     private ListView habitsListView;
     private HabitHistoryActivity.HabitHistoryAdapter habitsAdapter;
+
+    private FusedLocationProviderClient mFusedLocationClient;
 
     public DataSource<Habit> habitDataSource;
     public DataSource<HabitEvent> eventDataSource;
     public ArrayList<Habit> habits;
     public ArrayList<HabitEvent> habitEvents;
     private Repository<HabitEvent> habitEventRepository;
+
+    private Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +98,7 @@ public class HabitHistoryActivity extends BaseDrawerActivity implements
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         final FilterDialogResultListener resultListener = this;
         final ImageButton filterButton =
@@ -242,5 +257,38 @@ public class HabitHistoryActivity extends BaseDrawerActivity implements
 
             return convertView;
         }
+    }
+
+    /**
+     * Filter by within 5km
+     */
+    private void filterByNearby() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+            return;
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+
+                        if (location != null) {
+                            // Logic to handle location object
+                            HabitHistoryActivity.this.location = location;
+                            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+
+                            // TODO: update the data source here with the list of friends
+                        } else {
+                            Toast.makeText(HabitHistoryActivity.this, "Error getting location",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
